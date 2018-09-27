3055fc544db461d8834f4fe38d835ee9fc1ed7aa
Dmitry <m8er_ed@mail.ru>
2018-09-27 13:46:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using matrixes;
+
+
+namespace working_with_matrixes_from_files
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+
+            if (args.Length > 0)
+            {
+                //входной параметр main 
+                path = args[0];
+            }
+            else
+            {
+                //входной параметр для ввода после начала работы программы, если в main ничего не было передано 
+                Console.WriteLine("Input the directory:");
+                path = Console.ReadLine();
+            }
+            
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            if (Directory.Exists(path))
+            {
+
+                Console.WriteLine("Checking directory:");
+                FileInfo[] files = dir.GetFiles();
+                if (files.Count() > 0) {
+                    foreach (FileInfo i in files)
+                    {
+                        Console.WriteLine("File:" + "\"" + i.Name + "\"");
+                    }
+                    foreach (FileInfo i in files)
+                    {
+                        Console.WriteLine("going into file: " + i.Name);
+                        operation(path, i.Name);
+                    }
+                }  
+                else
+                {
+                    Console.WriteLine("Directry is empty");
+                } 
+            }
+            else
+            {
+                Console.WriteLine("Directry does not exist");
+            }
+
+        }
+
+        static void operation(string path, string file)
+        {
+
+            Console.WriteLine("\n\n");
+
+            //full way to file
+            string way = path + @"\" + file;
+            //connecting to file
+            StreamReader sr = new StreamReader(way, System.Text.Encoding.Default);
+
+            //getting operation name
+            string opername = sr.ReadLine();
+            Console.WriteLine("Operation name: " + opername);
+
+            //getting out of empty spot between operation and matrix itself
+            string randomstring = sr.ReadLine();
+            
+
+            string newfilename = file + "_result.txt";
+            //writting every line of matrix into list
+            ArrayList a = filetolist(way,sr);
+            Console.WriteLine("Arraylist is ready");
+            Matrix matrixA = new Matrix(a);
+            Console.WriteLine("matrix is ready");
+            //multiply,add, subtract, transpose
+            switch (opername) {
+                case "multiply":
+                    Console.WriteLine("Reading next matrix");
+                    ArrayList b = filetolist(way, sr);
+                    Matrix matrixB  = new Matrix(b);
+                    Console.WriteLine("multipling");
+                    matrixA = operations.multiply(matrixA, matrixB);
+                    matrixA.fileprint(path +@"\"+ newfilename);
+                break;
+
+                case "add":
+                    Console.WriteLine("Reading next matrix");
+                    ArrayList c = filetolist(way, sr);
+                    Matrix matrixC = new Matrix(c);
+                    Console.WriteLine("Adding");
+                    operations.add(matrixA, matrixC);
+                    matrixA.fileprint(path + @"\" + newfilename);
+                break;
+
+                case "subtract":
+                    Console.WriteLine("Reading next matrix");
+                    ArrayList d = filetolist(way, sr);
+                    Matrix matrixD = new Matrix(d);
+                    Console.WriteLine("Substracting");
+                    operations.substract(matrixA, matrixD);
+                    matrixA.fileprint(path + @"\" + newfilename);
+                break;
+
+                case "transpose":
+                    Console.WriteLine("Transponsing");
+                    matrixA =operations.transpose(matrixA);
+                    matrixA.fileprint(path + @"\"+ newfilename);
+                    while (sr.Peek() > -1)
+                    {
+                        Console.WriteLine("Reading next matrix");
+                        ArrayList E = filetolist(way, sr);
+                        matrixA = new Matrix(E);
+                        Console.WriteLine("Transponsing");
+                        matrixA=operations.transpose(matrixA);
+                        matrixA.fileprint(path + @"\" + newfilename);
+                    }
+                break;
+            
+            }
+            Console.WriteLine("All Operations are done");
+
+
+        }
+
+        static ArrayList filetolist(string path, StreamReader sr)
+        {
+            ArrayList a = new ArrayList();
+            //убираем пустую строчку после названия операции
+            string line_of_matrix = sr.ReadLine();
+            Console.WriteLine("Reading data form file\n");
+
+            while (line_of_matrix != null && line_of_matrix!=string.Empty)
+            {
+
+                a.Add(line_of_matrix);
+                line_of_matrix = sr.ReadLine();
+            }
+
+            return a;
+        }
+
+    }
+}
+
+
---
+++ matrixes.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+
+namespace matrixes
+{
+    public class Matrix
+    {
+        private int[,] matrix;
+        private int N;
+        private int M;
+
+
+        public int getN()
+        {
+            return this.N;
+        }
+
+        public int getM()
+        {
+            return this.M;
+        }
+
+        public int GetElement(int i,int j)
+        {
+            return matrix[i,j];
+        }
+        public void SetElement(int i, int j, int x)
+        {
+            this.matrix[i, j] = x;
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < this.N; i++)
+            {
+                for (int j = 0; j < this.M; j++)
+                {
+                    Console.Write(matrix[i,j]+" ");
+                }
+
+                Console.WriteLine('\n');
+            }
+               
+
+        }
+
+        public void AddElement(int i, int j, int x)
+        {
+            matrix[i,j] += x;
+        }
+
+        public void swap(int i, int j)
+        {
+            int temp = matrix[i,j];
+            matrix[i,j] = matrix[j,i];
+            matrix[j,i] = temp;
+
+        }
+
+        public Matrix(int N, int M)
+        {
+            matrix = new int[N,M];
+            this.M = M;
+            this.N = N;
+
+        }
+
+        public Matrix (ArrayList a)
+        {
+
+            int [] tempint=NumsFromString(a[0].ToString());
+            this.N = a.Count;
+            this.M = tempint.Length;
+            this.matrix = new int[N, M];
+            arraytoline(tempint, 0);
+
+            if (N > 1)
+            {
+                for (int i = 1; i < N; ++i)
+                {
+                    //вытаскиваем строку из списка
+
+                    tempint = NumsFromString(a[i].ToString());
+                    //записываем строку в массив
+                    arraytoline(tempint, i);
+                }
+            }
+           
+        }
+
+        public void fileprint(string path)
+        {
+            Console.WriteLine("Writting result into file:" +"\""+path+ "\"");
+
+            for (int i = 0; i < this.N; i++)
+            {
+                for (int j = 0; j < this.M; j++)
+                {
+                    System.IO.File.AppendAllText(path, matrix[i,j]+" ");
+                }
+
+                System.IO.File.AppendAllText(path, "\r\n");
+
+            }
+
+            System.IO.File.AppendAllText(path, "\r\n");
+        }
+
+
+        
+        private void arraytoline(int[] array,int line)
+        {
+            for (int i=0; i<array.Length; ++i)
+            {
+                matrix[line, i] = array[i];
+            }
+        }
+
+
+        //fuction that takes numbers from string into array
+        //numbers are separated with spacebars
+
+        private int [] NumsFromString(String a)
+        {
+            String [] b =a.Split(' ');
+            int[] Num = new int[b.Length];
+           
+            for (int i = 0; i < Num.Length; ++i)
+            {
+                Console.WriteLine("Getting element:"+ b[i].TrimEnd(' '));
+                Num[i] = Convert.ToInt32(b[i]);
+            }
+
+            return Num;
+        }
+               
+    }
+
+    public class operations
+    {
+        public static Matrix multiply(Matrix A, Matrix B)
+        {
+            //if (A.getM() != B.getN()) return false;
+
+            int n = A.getN();
+            int m = A.getM();
+            int l = B.getM();
+            Matrix C = new Matrix(A.getN(), B.getM());
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < l; j++)
+                    for (int k = 0; k < m; k++)
+                        C.AddElement(i, j, A.GetElement(i, k) * B.GetElement(k,j));
+
+            return C;
+
+        }
+
+        public static bool add(Matrix A, Matrix B)
+        {
+            if (A.getN() != B.getN() || A.getM() != B.getM())
+            {
+                return false;
+            }
+                            
+
+            for (int i = 0; i < A.getN(); i++)
+                for (int j = 0; j < A.getM(); j++)
+                    A.AddElement(i, j, B.GetElement(i, j));
+
+            return true;
+
+        }
+
+        public static bool substract(Matrix A, Matrix B)
+        {
+
+            if (A.getN() != B.getN() || A.getM() != B.getM()) return false;
+            for (int i = 0; i < A.getN(); i++)
+                for (int j = 0; j < A.getM(); j++)
+                    A.AddElement(i, j, -1*B.GetElement(i, j));
+
+            return true;
+        }
+
+        public static Matrix transpose(Matrix A)
+        {
+            if (A.getM() == A.getN())
+            {
+            
+                for (int i = 0; i < A.getN(); i++)
+                    for (int j = i; j < A.getM(); j++)
+                        A.swap(i, j);
+
+                return A;
+            }
+            else
+            {
+             
+                Matrix B = new Matrix(A.getM(), A.getN());
+                for (int i = 0; i < A.getN(); i++)
+                    for (int j = 0; j < A.getM(); j++)
+                        B.SetElement(j, i, A.GetElement(i, j));
+                                                
+                return B;
+            }
+        }
+
+    }
+
+}
+
